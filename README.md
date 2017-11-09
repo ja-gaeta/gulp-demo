6c1b14993d19e055ae632a78cae7082a7ea1d76d
Jose Mendes <ja.gaeta@hotmail.com>
2017-11-09 11:46:44 -0200

    Adicionada segunda parte - javascript

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 O artigo completo com as instruções para uso do Gulp e criação dos arquivos contidos neste repositório pode ser lido [aqui](https://pontogpp.com.br/blog/video/relampago-video-02/).
 
-O tutorial também poderá ser assistido em vídeo no [YouTube](https://youtu.be/r6LpCzO6OW8).
+O tutorial também poderá ser assistido em vídeo no YouTube:
+
+ [Parte 1](https://youtu.be/r6LpCzO6OW8)
+ [Parte 2](https://youtu.be/OnBWQvRGRH4)
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var
   gulp = require('gulp'),
   newer = require('gulp-newer'),
   imagemin = require('gulp-imagemin'),
+  plumber = require('gulp-plumber'),
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify')
+
+// modo de desenvolvimento?
+devBuild = true
 
 // folders
 folder = {
@@ -18,3 +26,17 @@ gulp.task('images', function() {
     .pipe(imagemin({ optimizationLevel: 5 }))
     .pipe(gulp.dest(out));
 });
+
+// processamento dos arquivos JavaScript
+gulp.task('js', function() {
+    var jsbuild = gulp.src(folder.src + 'js/**/*')
+      .pipe(plumber())
+      .pipe(deporder())
+      .pipe(concat('all.js')); 
+    if (!devBuild) {
+      jsbuild = jsbuild
+        .pipe(stripdebug())
+        .pipe(uglify());
+    }
+    return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+  });
